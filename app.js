c5eae215ccac45a31e712333ad9605eb091894d9
leprincehouba-del <leprince.houba@gmail.com>
2026-07-17 16:55:31 +0300

    Add auto login

--- app.js
+++ app.js
@@ -412,3 +412,4 @@ populateSelects();
 loadAttendance();
 togglePointsFields();
 if("serviceWorker" in navigator){window.addEventListener("load",()=>navigator.serviceWorker.register("service-worker.js").catch(()=>{}));}
+checkSession();
