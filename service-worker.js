93baf0e46d4a5eb222b758729c685012f1a03632
leprincehouba-del <leprince.houba@gmail.com>
2026-07-17 23:19:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="ef-academy-v1";
+const CACHE="ef-academy-v2";
 const ASSETS=["./","index.html","styles.css","app.js","logo.png","manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
